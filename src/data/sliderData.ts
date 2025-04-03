4fd9f7cc0abcf9a0958e0d10ae2da73a88b03213
Chepolocote <158088835+Chepolocote@users.noreply.github.com>
2025-04-02 21:24:59 -0700

    ultimos cambios

--- src/data/sliderData.ts
+++ src/data/sliderData.ts
@@ -1,6 +1,6 @@
-import slider1 from '../assets/image/slider-1.jpg';
-import slider2 from '../assets/image/slider-2.jpg';
-import slider3 from '../assets/image/slider-3.jpg';
+import slider1 from '../assets/image/slider-1.png';
+import slider2 from '../assets/image/slider-2.png';
+import slider3 from '../assets/image/slider-3.png';
 
 interface ISliderData {
   imgUrl: string;
