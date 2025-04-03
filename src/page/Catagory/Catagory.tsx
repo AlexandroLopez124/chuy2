2c9881773424ead9d23af9aebbfd22f0760ea958
Chepolocote <158088835+Chepolocote@users.noreply.github.com>
2025-04-02 18:29:05 -0700

    cambios slider

--- src/page/Catagory/Catagory.tsx
+++ src/page/Catagory/Catagory.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from '../../Layout';
-import phoneImage from '../../assets/image/phone-catagory.jpg';
-import consoleImage from '../../assets/image/console-catagory.jpg';
-import accImage from '../../assets/image/acc-catagory.jpg';
-import headphoneImage from '../../assets/image/headphone-catagory.jpg';
+import phoneImage from '../../assets/image/concacaf.png';
+import consoleImage from '../../assets/image/uefa.png';
+import accImage from '../../assets/image/conmebol.png';
+import headphoneImage from '../../assets/image/fifa.jpg';
 import './Catagory.css';
 
 const Catagory: FC = () => {
@@ -13,19 +13,19 @@ const Catagory: FC = () => {
       <div className='catagory'>
         <Link className='catagory__link' to='phone'>
           <img className='catagory__image' src={phoneImage} alt="phone's picture" />
-          <h4 className='catagory__text'>Phone</h4>
+          <h4 className='catagory__text'>CONCACAF</h4>
         </Link>
         <Link className='catagory__link' to='headphone'>
-          <img className='catagory__image' src={headphoneImage} alt="phone's picture" />
-          <h4 className='catagory__text'>HeadPhone</h4>
+          <img className='catagory__image' src={consoleImage} alt="phone's picture" />
+          <h4 className='catagory__text'>UEFA</h4>
         </Link>
         <Link className='catagory__link' to='accessories'>
           <img className='catagory__image' src={accImage} alt="phone's picture" />
-          <h4 className='catagory__text'>Accessories</h4>
+          <h4 className='catagory__text'>CONMEBOL</h4>
         </Link>
         <Link className='catagory__link' to='consols'>
-          <img className='catagory__image' src={consoleImage} alt="phone's picture" />
-          <h4 className='catagory__text'>Consoles</h4>
+          <img className='catagory__image' src={headphoneImage} alt="phone's picture" />
+          <h4 className='catagory__text'>SELECCIONES</h4>
         </Link>
       </div>
     </Container>
