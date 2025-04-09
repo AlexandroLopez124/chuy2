4c07bd0abcb780f29d0815b56a48c1977866dc17
Aksnesz <wmoreno2004@gmail.com>
2025-04-09 02:28:09 -0600

    Agregar pasarela de pago stripe

---
+++ src/config/stripeConfig.ts
@@ -0,0 +1,4 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51P2PCqLOoj4xj39ToufxgpExh9oR33230bXeU3c16AHA0Zt9rCa2BtaJOPjUl91B1dkwTkqfhF8ECXGzN2OjWFUN00yeRGCIRX');
+export default stripePromise;
